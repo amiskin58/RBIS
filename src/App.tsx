1803f7723c8f77380726b8178de7e03f08355e7d
高勤 <kdj.no1@gmail.com>
2026-07-18 07:19:05 +0800

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <MainLayout>
         <AppRouter />
       </MainLayout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
